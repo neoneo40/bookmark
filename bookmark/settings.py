fae7cbaabee68daf01935baf5769486cef05b73b
re4lfl0w <re4lfl0w@gmail.com>
2019-06-20 05:33:24 +0900

    Modify allowed hosts

--- bookmark/settings.py
+++ bookmark/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^y5gwj4n_&9f^l7=sr6v*c$ack$8+$g)yd$0rc__0wyz0y*w6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
